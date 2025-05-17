988692fa338190b5a9fba256ce29b6afdf441e52
ogkourlias <orfeasgkourlias@gmail.com>
2025-05-17 12:00:30 +0200

    Worked up until the end (Chroloplast implementation) of part 3 of assignment 2

--- assignment2/assignment2.py
+++ assignment2/assignment2.py
@@ -50,19 +50,64 @@ class Molecule():
 
     def __str__(self):
         return self.symbol
+
+class Chroloplast:
+    def __init__(self):
+        self.water = 0
+        self.co2 = 0
+        self.sugar = 0
+        self.o2 = 0
     
+    def add_molecule(self, molecule):
+        if str(molecule) == "H2O":
+            self.water += 1
+        elif str(molecule) == "CO2":
+            self.co2 += 1
+        else:
+            raise ValueError("Not H2O or CO2")
+        if self.water >= 12 and self.co2 >= 6:
+            self.water -= 12
+            self.co2 -= 6
+            hydrogen = Atom("H", 1, 0)
+            carbon = Atom("C", 6, 6)
+            oxygen = Atom("O", 8, 8)
+            sugar = Molecule( [ (carbon, 6), (hydrogen, 12), (oxygen, 6) ] )
+            o2 = Molecule([(oxygen, 2)])
+            self.sugar += 1
+            self.o2 += 6
+            return [(str(sugar), self.sugar), (str(o2), self.o2)]
+        return []
+
+    def __str__(self):
+        return f"H2O: {self.water} \n CO2: {self.co2}"
+
 # MAIN
 def main(args):
     """ Main function """
     hydrogen = Atom('H', 1, 0)
     carbon = Atom('C', 6, 6)
     oxygen = Atom('O', 8, 8)
-
     water = Molecule( [ (hydrogen, 2), (oxygen, 1) ] )
     co2 = Molecule( [ (carbon, 1), (oxygen, 2) ])
-    print (water) # H2O
-    print (co2) # CO2
-    print (water + co2) # H2OCO2
+    demo = Chroloplast()
+    els = [water, co2]
+    while (True):
+        print ('\nWhat molecule would you like to add?')
+        print ('[1] Water')
+        print ('[2] carbondioxyde')
+        print ('Please enter your choice: ', end='')
+        try:
+            choice = int(input())
+            res = demo.add_molecule(els[choice-1])
+            if (len(res)==0):
+                print (demo)
+            else:
+                print ('\n=== Photosynthesis!')
+                print (res)
+                print (demo)
+
+        except Exception:
+            print ('\n=== That is not a valid choice.')
     # FINISH
     return 0
 
