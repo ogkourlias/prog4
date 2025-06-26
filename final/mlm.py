b6a8f777663181af94e8f9d9d44ca14d0ea4ed9e
ogkourlias <orfeasgkourlias@gmail.com>
2025-06-26 21:43:31 +0200

    Removed unusued imports and added prediction method

--- final/mlm.py
+++ final/mlm.py
@@ -11,44 +11,33 @@ __status__ = "WIP"
 __version__ = "0.1"
 
 # IMPORTS
-import sys
-import argparse
-import gzip
-import pathlib
-import multiprocessing as mp
-import time
-import pandas as pd
 from sklearn.preprocessing import StandardScaler
-from sklearn.svm import OneClassSVM
-from sklearn.covariance import EllipticEnvelope
-from sklearn.ensemble import IsolationForest
 from sklearn.neighbors import LocalOutlierFactor
 
-def argparser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--input", "-i", required=True, type=pathlib.Path)
-    return parser.parse_args()
-
 # CLASSES
-
 class MLM:
     def __init__(self, df):
         self.df = df
 
     def split(self):
-        self.df = self.df.set_index('timestamp')
         self.train_df = self.df[(self.df.index >= "2018-04-01") & (self.df.index < "2018-07-01")]
         self.test_df = self.df[(self.df.index >= "2018-07-01") & (self.df.index < "2018-09-01")]
-    
+
     def training_process(self):
-        self.train_df = self.train_df.drop(['sensor_15', 'sensor_50'],axis=1)
-        normal_rows = self.train_df[self.train_df['machine_status']=='NORMAL']
-        self.numerical_cols = self.train_df.columns[:-1]
-        self.train_df[self.numerical_cols]=self.train_df[self.numerical_cols].fillna(self.train_df[self.numerical_cols].mean())
-        self.test_df[self.numerical_cols]=self.test_df[self.numerical_cols].fillna(self.test_df[self.numerical_cols].mean())
-        scaler=StandardScaler()
+        self.numerical_cols = self.df.drop(columns=['machine_status']).columns
+
+        # Filter rows with normal machine status
+        normal_rows = self.train_df[self.train_df['machine_status'] == 'NORMAL']
+
+        # Standardize numerical features
+        scaler = StandardScaler()
         self.train_df[self.numerical_cols] = scaler.fit_transform(self.train_df[self.numerical_cols])
-        outliers_fraction = 1 - (len(normal_rows) / len(self.train_df))
+        self.test_df[self.numerical_cols] = scaler.transform(self.test_df[self.numerical_cols])
+
+        # Calculate outlier fraction
+        outliers_fraction = 1.0 - (len(normal_rows) / len(self.train_df))
+
+        # Return the processed numerical features
         numerical_df = self.train_df[self.numerical_cols]
         return outliers_fraction, numerical_df
 
@@ -57,15 +46,12 @@ class MLM:
         outliers_fraction, numerical_df = self.training_process()
         alg = LocalOutlierFactor(novelty=True, contamination=outliers_fraction, n_jobs=-1)
         self.model = alg.fit(numerical_df)
-        self.test_df["LocalOutlierFactor"] = self.model.predict(self.test_df[self.numerical_cols])
-        return self.test_df
+        # self.test_df["LocalOutlierFactor"] = self.model.predict(self.test_df[self.numerical_cols])
+        return self
 
+    def predict_df(self, df):
+        df["LocalOutlierFactor"] = self.model.predict(df[self.numerical_cols])
+        return df
+    
     def run(self):
         return self.train()
-
-
-def main():
-    pass
-
-if __name__ == '__main__':
-    main()
