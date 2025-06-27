5ce7195a04ed5364e7e9bc0ccfb358f8ac4b74c0
ogkourlias <orfeasgkourlias@gmail.com>
2025-06-27 10:41:09 +0200

    Added thread number selection to CLI

--- final/final.py
+++ final/final.py
@@ -11,25 +11,28 @@ __status__ = "WIP"
 __version__ = "0.1"
 
 # IMPORTS
+import os
 import argparse
 import pathlib
 from file_handler import FileHandler
 from mlm import MLM
 from watcher import Watcher
 import pandas as pd
-pd.options.mode.chained_assignment = None  # default='warn'
+pd.options.mode.chained_assignment = None  # default="warn"
 
 def argparser():
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", "-i", required=True, type=pathlib.Path)
     parser.add_argument("--output", "-o", required=True, type=pathlib.Path)
     parser.add_argument("--trainfile", "-t", required=True, type=pathlib.Path)
+    parser.add_argument("--num-threads", "-n", default=1, type=int)
     return parser.parse_args()
 
 def main():
     args = argparser()
+    os.environ["OPENBLAS_NUM_THREADS"] = args.num_threads
     df = FileHandler(args.trainfile).run()
-    model = MLM(df).run()
+    model = MLM(df, args.num_threads).run()
     Watcher(model, args.input, args.output).run()
 
 if __name__ == '__main__':
